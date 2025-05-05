8b10d83deb8caefe9a1b39067cb562fe562fbf6a
mazagri3 <obrida@live.com>
2025-05-05 16:30:55 -0400

    Fix TypeScript configuration and update build process

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'vj1s6i',
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:3007',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
