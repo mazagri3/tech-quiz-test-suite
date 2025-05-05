88527969ca6ad3257892a86025b1dfc40c6f3065
mazagri3 <obrida@live.com>
2025-05-05 16:03:00 -0400

    Add starter code and Cypress configuration

---
+++ cypress/support/commands.ts
@@ -0,0 +1,5 @@
+// Import commands.js using ES2015 syntax:
+import '@testing-library/cypress/add-commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('@testing-library/cypress/add-commands') 
